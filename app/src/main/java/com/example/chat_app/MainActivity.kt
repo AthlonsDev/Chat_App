e61359863a201757063e5bf365c912626d3c3c6e
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-04-27 18:59:46 +0100

    Simple Dummy Login/Register Screens using 2 different layouts and classes with connected wit Intent

--- app/src/main/java/com/example/chat_app/MainActivity.kt
+++ app/src/main/java/com/example/chat_app/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.chat_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        registe_button.setOnClickListener {
+            val email = email_edtText.text.toString()
+            val pass = pass_editText.text.toString()
+
+            Log.d("mainact", "email is: $email")
+            Log.d("mainact", "password is: $pass")
+        }
+
+        already_have_account.setOnClickListener {
+            Log.d("mainact", "Try to show login activity")
+
+//            launch login activity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
