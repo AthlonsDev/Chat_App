4c71203b44100ab6178330596f088c07f389d2a1
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-04-28 21:30:55 +0100

    Added firebase auth register user more ui works

--- app/src/main/java/com/example/chat_app/LoginActivity.kt
+++ app/src/main/java/com/example/chat_app/LoginActivity.kt
@@ -2,7 +2,11 @@ package com.example.chat_app
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity: AppCompatActivity() {
 
@@ -10,6 +14,26 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener{
+            val email = email_edtText.text.toString()
+            val password = pass_editText.text.toString()
+
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            }
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener{
+                    if(it.isSuccessful) {
+
+                    }
+
+                }
+                .addOnFailureListener{
+                    Toast.makeText(this, "Failed to login user: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+        }
     }
 
 }
--- app/src/main/java/com/example/chat_app/MainActivity.kt
+++ app/src/main/java/com/example/chat_app/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +18,7 @@ class MainActivity : AppCompatActivity() {
 
 
         registe_button.setOnClickListener {
-            val email = email_edtText.text.toString()
-            val pass = pass_editText.text.toString()
-
-            Log.d("mainact", "email is: $email")
-            Log.d("mainact", "password is: $pass")
+          performRegister()
         }
 
         already_have_account.setOnClickListener {
@@ -29,6 +28,32 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+    }
+
+
+    fun performRegister() {
+        val email = email_edtText.text.toString()
+        val pass = pass_editText.text.toString()
+
+        if(email.isEmpty() || pass.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+        }
+
+        Log.d("mainact", "email is: $email")
+        Log.d("mainact", "password is: $pass")
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener{
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("firebase", "Successfully created user with uid: ${it.result.user?.uid}")
+                Toast.makeText(this, "Successfully created user with uid: ${it.result.user?.uid}", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Log.d("firebase", "Failed to Create user: ${it.message}")
+                Toast.makeText(this, "Failed to Create user: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
 
     }
 }
