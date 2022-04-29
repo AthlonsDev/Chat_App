42a3b517bed82cb3fafe706b4b9aa37f8579e675
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-04-29 23:38:01 +0100

    Working on uploading images

--- app/src/main/java/com/example/chat_app/RegisterActivity.kt
+++ app/src/main/java/com/example/chat_app/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.example.chat_app
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -29,6 +32,23 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        select_photo_button.setOnClickListener{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            val uri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            select_photo_button.setBackgroundDrawable(bitmapDrawable)
+        }
     }
 
 
